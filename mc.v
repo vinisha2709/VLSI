409e49e5853d4aa204af616a72e88af8c34da583
vinisha2709 <115263829+vinisha2709@users.noreply.github.com>
2023-07-29 11:31:51 +0530

    Create mc.v

---
+++ mc.v
@@ -0,0 +1,21 @@
+//20T118 ROSELIN VINISHA R
+module mc(
+input [3:0]a,b,
+    output reg e,g,l
+    );
+always @ (a,b)
+    begin 
+        if (a < b)
+		  begin
+            g=0; e=0; l=1;
+				end  
+        else if (a == b)
+		  begin
+            g=0; e=1; l=0;    
+			end  				
+		  else
+		  begin
+           g=1; e=0; l=0;
+			  end  
+end  
+endmodule
