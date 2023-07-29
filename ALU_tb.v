0e41223cbd2f1b16813c028e97f555046aae1b18
vinisha2709 <115263829+vinisha2709@users.noreply.github.com>
2023-07-29 10:09:32 +0530

    Create ALU_tb.v

---
+++ ALU_tb.v
@@ -0,0 +1,35 @@
+//20T118
+module ALU_tb;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+	reg [2:0] s;
+
+	// Outputs
+	wire [7:0] y;
+
+	// Instantiate the Unit Under Test (UUT)
+	ALU uut (
+		.a(a), 
+		.b(b), 
+		.s(s), 
+		.y(y)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 4'b1001;b = 4'b0011;s = 3'b000;
+
+		// Wait 100 ns for global reset to finish
+		#100 a = 4'b1001;b = 4'b1011;s = 3'b001;
+		#100 a = 4'b1001;b = 4'b0111;s = 3'b010;
+		#100 a = 4'b1101;b = 4'b1011;s = 3'b011;
+		#100 a = 4'b1011;b = 4'b0111;s = 3'b100;
+		#100 a = 4'b1010;b = 4'b1011;s = 3'b101;
+		#100 a = 4'b1110;b = 4'b0111;s = 3'b110;
+		#100 a = 4'b0101;b = 4'b1011;s = 3'b111;
+
+	end
+      
+endmodule
