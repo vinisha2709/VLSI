e245db54a35b5af3f1f2aa13a785405bb2f37771
vinisha2709 <115263829+vinisha2709@users.noreply.github.com>
2023-07-31 13:48:38 +0530

    Update FSM1.v

--- FSM/FSM1.v
+++ FSM/FSM1.v
@@ -1,46 +1,50 @@
 //20T118
 
 module FSM1(
-    input rst,clk,x,
+     input clk,rst,x,
     output reg y
     );
-parameter s0=2'b00;
-parameter s1=2'b01;
-parameter s2=2'b10;
-parameter s3=2'b11;
-reg [1:0] nxt,pre;
-always @(posedge clk,posedge rst)
-begin
-   if(rst) begin
-	   pre=s0;
-	end
-   else
-      pre=nxt;	
-end
-always @(pre,x)
-begin
-   case(pre)
-	s0: begin 
-	    y=1'b0;
-	    if(x) nxt=s1;
-	    else nxt=s0;
-	end
-	s1: begin
-	    y=1'b0;
-		 if(x) nxt=s2;
-		 else nxt=s1;
-	end
-   s2: begin
-      y=1'b0;
-      if(x) nxt=s3;
-      else	nxt=s2;
-   end
-   s3: begin
-      y=1'b1;
-      if(x) nxt=s0;
-      else nxt=s3;
-   end
-   endcase
+parameter A = 2'b00;
+parameter B = 2'b01;
+parameter C = 2'b10;
+parameter D = 2'b11;
+reg [1:0]p,n;
 
-end	
+always @(posedge clk or posedge rst)
+begin
+	if(rst) p = A;
+	else
+		begin
+			case({p})
+				A : 
+					begin
+						y = 0;
+						if(x) n = B ;
+						else n = A;
+					end
+				B : 
+					begin
+						y = 0;
+						if(x) n = C;
+						else n = B;
+					end
+				C : 
+					begin
+						y = 0;
+						if(x) n = D;
+						else n = C;
+					end
+				D : 
+					begin
+						y = 1;
+						if(x) n = A;
+						else n = D;
+					end
+			endcase
+			p = n;
+		end
+	end
+
+endmodule
+
 endmodule
